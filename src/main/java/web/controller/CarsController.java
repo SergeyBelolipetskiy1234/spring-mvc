72ac7143cd836273e3f0134ed0ef15c7b64de2d8
SergeyBelolipetskiy1234 <89917779+SergeyBelolipetskiy1234@users.noreply.github.com>
2021-10-06 20:49:42 +0300

    update code style. 06.10.21

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -13,31 +13,20 @@ import java.util.List;
 
 @Controller
 public class CarsController {
-    @Autowired
+
     private CarService carService;
+
+    @Autowired
+    public CarsController(CarService carService) {
+        this.carService = carService;
+    }
 
     @GetMapping(value = "/cars")
     public String getCars(@RequestParam(value = "count", required = false) Integer count, Model model) {
-        Car car1 = new Car("Kia", 3, "black");
-        Car car2 = new Car("Ford", 2, "white");
-        Car car3 = new Car("Toyota", 3, "red");
-        Car car4 = new Car("Honda", 4, "gray");
-        Car car5 = new Car("Heyndai", 3, "black");
-        List<Car> list = new ArrayList<Car>();
-        list.add(car1);
-        list.add(car2);
-        list.add(car3);
-        list.add(car4);
-        list.add(car5);
+        List<Car> list = carService.getList();
 
-        List<Car> result = null;
-        if(count == null) {
-            result = list;
-        } else if (count != null) {
-            result = carService.getOfCar(list, count);
-        }
 
-        model.addAttribute("cars", result);
+        model.addAttribute("cars", carService.getOfCar(list, count));
         return "cars";
 
     }
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -3,10 +3,31 @@ package web.service;
 import org.springframework.stereotype.Service;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CarService {
+
+    List<Car> list = new ArrayList<Car>();
+
+    public List<Car> getList() {
+        return list;
+    }
+
+    Car car1 = new Car("Kia", 3, "black");
+    Car car2 = new Car("Ford", 2, "white");
+    Car car3 = new Car("Toyota", 3, "red");
+    Car car4 = new Car("Honda", 4, "gray");
+    Car car5 = new Car("Heyndai", 3, "black");
+
+    {
+        list.add(car1);
+        list.add(car2);
+        list.add(car3);
+        list.add(car4);
+        list.add(car5);
+    }
 
     public List<Car> getOfCar(List<Car> list, int count) {
         List<Car> result;
@@ -25,7 +46,6 @@ public class CarService {
                 break;
             default:
                 result = list;
-
         }
         return result;
     }
